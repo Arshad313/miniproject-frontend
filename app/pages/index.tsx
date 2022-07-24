bea500e39f245a7ae23fd232731c42ccac589256
Abin Joseph <itsmeabjs@gmail.com>
2022-07-24 14:47:26 +0530

    docs: update home page

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,9 +1,12 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable react/no-unknown-property */
 import { Suspense } from "react"
 import { Image, Link, BlitzPage, useMutation, Routes } from "blitz"
 import Layout from "app/core/layouts/Layout"
 import { useCurrentUser } from "app/core/hooks/useCurrentUser"
 import logout from "app/auth/mutations/logout"
 import logo from "public/logo.png"
+import { Button, Input } from "@mui/material"
 
 /*
  * This file is just for a pleasant getting started page for your new app.
@@ -17,22 +20,24 @@ const UserInfo = () => {
   if (currentUser) {
     return (
       <>
-        <button
-          className="button small"
-          onClick={async () => {
-            await logoutMutation()
-          }}
-        >
-          Logout
-        </button>
-        <div>
-          User id: <code>{currentUser.id}</code>
-          <br />
-          User role: <code>{currentUser.role}</code>
-          <br />
-          User email: <code>{currentUser.email}</code>
+        <div className="flex justify-evenly items-center w-screen">
+          <img src="/logo-book.jpg" alt="" className="w-24" />
+          <Button
+            className=""
+            onClick={async () => {
+              await logoutMutation()
+            }}
+          >
+            Logout
+          </Button>
         </div>
-        User data: <code>{JSON.stringify(currentUser)}</code>
+        <div className="h-80 bg-slate-300 w-96 ml-52 mt-20 rounded-lg items-center justify-center">
+          <h1 className="text-center p-5 font-mono text-lg">Search For Books</h1>
+          <Input className="w-full p-3" placeholder="Enter A Title:" />
+          <Input className="w-full p-3" placeholder="Enter An Author:" />
+          <Button>Search</Button>
+          <Button>Reset Fields</Button>
+        </div>
       </>
     )
   } else {
@@ -55,7 +60,7 @@ const UserInfo = () => {
 
 const Home: BlitzPage = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center h-screen w-screen">
       <div className="buttons" style={{ marginTop: "1rem", marginBottom: "1rem" }}>
         <Suspense fallback="Loading...">
           <UserInfo />
